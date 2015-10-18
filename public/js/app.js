ce2d228052bb6113e41eb32c7056371afe166728
Jesus A Lopez <jesus.alberto.lopez@outlook.es>
2015-10-17 23:26:57 -0600

    Modifiacdo add layout

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,27 @@
 
 $('.Menu-item').click(notification)
 $('.prevent').click(preventDefault)
+$('.Location-mobileDrop').click(addActive)
+$('.Location-mobileDrop').click(scrollTo)
+function scrollTo(e){
+	$id=$(this).attr('data-scroll')
+	$('html, body').animate({
+        scrollTop: $('#'+$id).offset().top
+    }, 300);}
+function prueba(e){
+	console.log('jejee')
+}
+function addActive(e){
+	e.stopPropagation()
+	$(this).toggleClass('active')
+	$(this).children('.disable, .active').toggleClass('disable active')
+}
 function notification(e){
 	e.stopPropagation()
 	$parents = $(this).parents('*')
 	$(this).toggleClass('active')
 	$(this).find('.icon-menu').toggleClass('active')
-	$(this).parents('.Menu-option').siblings().find('.Menu-item').removeClass('active')
+	$(this).parents('.Menu-option').siblings().find('*').removeClass('active')
 	$(this).siblings().removeClass('active')
 	$(this).find('.notifcount').removeClass('pending')
 	$parents.click(close)
